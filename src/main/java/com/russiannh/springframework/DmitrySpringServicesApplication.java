d0a667d8b19207689a8193ae05910a16d00e764b
Kolomyitsev <dmitry.kolomyitsev@maine.gov>
2021-12-07 13:53:54 -0500

    initial commit

---
+++ src/main/java/com/russiannh/springframework/DmitrySpringServicesApplication.java
@@ -0,0 +1,13 @@
+package com.russiannh.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmitrySpringServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmitrySpringServicesApplication.class, args);
+	}
+
+}
